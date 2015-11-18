54929895d3ed9c60093b449a99f14e1a1b127e84
luna777 <xinguan777@gmail.com>
2015-11-18 01:20:51 -0500

    added comments.

--- CISC3150--HW7/src/HW7Q1.java
+++ CISC3150--HW7/src/HW7Q1.java
@@ -11,21 +11,20 @@ public class HW7Q1 {
     	
     }
     
+    //recursive function to get every directories and print them out in tree format.
     public static void printDir(String s, FileWriter fw, int a) throws IOException{
-    	//0x251C├ 0x2514└ 0x2500─
+    	//Ascii extend code: 0x2514└ 0x2500─
     	File current = new File(s);
     	File[] dirs = current.listFiles();
     	
+    	//print out the folders in current directory
     	for(int i=0; i<dirs.length; i++){
     		if(dirs[i].isDirectory()){
     			
     			for(int j=0; j<a-1; j++){
-    				//System.out.print("   ");
     				fw.write("   ");
     			}
-    			//System.out.println("└──"+dirs[i].getName());
     			fw.write("└──"+dirs[i].getName()+ "\n");
-    			//printDir(file.getCanonicalPath(), fw, a+1);
     			printDir(dirs[i].getPath(), fw, a+1);
     			
     			fw.flush();
